a558d727ed2ce97fb3aa182ecd8d1e82cb4fdec9
Wei Bo Zheng <47736792+a076902768@users.noreply.github.com>
2022-10-06 18:24:25 +0800

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
+const routerBase =
+  // process.env.DEPLOY_ENV === 'GH_PAGES'
+  true
+    ? {
+      router: {
+        base: '/test/'
+      }
+    }
+    : {}
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  ...routerBase
 })
